c60620555e8c0cd3e3b3a8a0f045653d482b3199
ljy1348 <141599648+ljy1348@users.noreply.github.com>
2023-09-19 15:47:09 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 2.42 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        int[] answer = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int min = 1000001;
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                if (arr[j] > queries[i][2]) {
+                    if (min > arr[j]) min = arr[j];
+                }
+            }
+            if (min == 1000001) answer[i] = -1;
+            else answer[i] = min;
+        }
+        
+        return answer;
+    }
+}
