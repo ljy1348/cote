3921d2bc23d4556db53c093025849ee7600bc970
ljy1348 <141599648+ljy1348@users.noreply.github.com>
2023-09-21 11:53:47 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+        answer = numStr.indexOf(kStr)+1;
+        if (answer == 0) answer = -1;
+        return answer;
+    }
+}
