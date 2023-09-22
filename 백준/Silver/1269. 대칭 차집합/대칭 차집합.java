0e9104a8fc987070e417517b88a2b41856469cb7
ljy1348 <141599648+ljy1348@users.noreply.github.com>
2023-09-22 15:31:02 +0900

    [Silver IV] Title: 대칭 차집합, Time: 720 ms, Memory: 69716 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * packageName : backjoon
+ * fileName : P1269
+ * author : GGG
+ * date : 2023-09-22
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2023-09-22         GGG          최초 생성
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] arr2 = new int[m];
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+            count++;
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                count--;
+                continue;
+            }
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
