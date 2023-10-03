2ec0d964ffdf43be1aff595b77aaefb40fb5e98c
ljy1348 <141599648+ljy1348@users.noreply.github.com>
2023-10-03 10:56:48 +0900

    [Silver I] Title: IOIOI, Time: 280 ms, Memory: 20152 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,24 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        String str = sc.next();
-        sc.close();
-        int start = 0;
-        int last = n*2+1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        String str = br.readLine();
         int result = 0;
-        while(last <= m) {
-            for (int i = start; i < last; i++) {
-                if ((i-start)%2==0 && str.charAt(i) != 'I') break;
-                if ((i-start)%2==1 && str.charAt(i) != 'O') break;
-                if (i == last-1) result++;
+        int count = 0;
+        for (int i = 1; i < m-1; i++) {
+            if (str.charAt(i) == 'O' && str.charAt(i+1) == 'I') {
+                count++;
+                if (count == n) {
+                    if (str.charAt(i - (count-1)*2 - 1) == 'I') {
+                        result++;
+                        count--;
+                    } else {
+                        count--;
+                    }
+                }
+                i++;
+            } else {
+                count = 0;
             }
-            start++;
-            last++;
         }
+
         System.out.println(result);
     }
 }
