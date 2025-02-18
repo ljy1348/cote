0c76068d72c14febe35c2cc60f4d1141d8505aa5
이주영 <141599648+ljy1348@users.noreply.github.com>
2025-02-19 05:00:30 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14852 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        dp[1] = arr[1];
+        if (n == 1) {
+            System.out.println(dp[1]);
+            return;
+        }
+        dp[2] = arr[1]+arr[2];
+        if (n == 2) {
+            System.out.println(dp[2]);
+            return;
+        }
+        dp[3] = Math.max(arr[2]+arr[3], arr[1]+arr[3]);
+        dp[3] = Math.max(dp[3], dp[2]);
+
+        if (n == 3) {
+            System.out.println(dp[3]);
+            return;
+        }
+        
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2]+arr[i]);
+            dp[i] = Math.max(dp[i], dp[i-3]+arr[i]+arr[i-1]);
+        }
+
+        System.out.println(dp[n]);
+
+    }
+
+}
