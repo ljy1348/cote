90c35033565ff0ce9a058964c70f2904c35af486
이주영 <141599648+ljy1348@users.noreply.github.com>
2025-07-17 09:11:58 +0900

    [Silver III] Title: 피보나치 함수, Time: 60 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,40 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-/**
- * packageName : backjoon
- * fileName : P1003
- * author : GGG
- * date : 2023-09-26
- * description : 피보나치 함수
- * 요약 :
- * <p>
- * ===========================================================
- * DATE            AUTHOR             NOTE
- * —————————————————————————————
- * 2023-09-26         GGG          최초 생성
- */
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String answer = "";
-        int[][] result = new int[41][2];
-            for (int j = 0; j <= 40; j++) {
-                if (j == 0) result[0][0]++;
-                else if (j == 1) {
-                    result[1][0] = 0;
-                    result[1][1] = 1;
-                } else {
-                    result[j][0] = result[j-1][0]+result[j-2][0];
-                    result[j][1] = result[j-1][1] + result[j-2][1];
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                int m = sc.nextInt();
-                if (i == n-1) answer += result[m][0] + " " + result[m][1];
-                else answer += result[m][0] + " " + result[m][1]+"\n";
-            }
-        System.out.println(answer);
-    }
 
+    static long[] arr = new long[41];
+    static long zero = 0L;
+    static long one = 0L;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= 40; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        while (--n >= 0) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                sb.append(1).append(" ").append(0).append("\n");
+            }  else  {
+                sb.append(arr[input-1]).append(" ").append(arr[input]).append("\n");    
+            }
+            
+        }
+        
+        System.out.println(sb);
+        
+    }
 }
