1b8274aee2fabd8e4c1d59df46f2602f1bc43da9
이주영 <141599648+ljy1348@users.noreply.github.com>
2025-07-17 13:56:16 +0900

    [Silver III] Title: 바이러스, Time: 80 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,48 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Array;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+    public static int count = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        ArrayList<Integer>[] list = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) {
-            list[i] = new ArrayList();
+        
+        int nodes = Integer.parseInt(br.readLine());
+        int edges = Integer.parseInt(br.readLine());
+        
+        
+        ArrayList<Integer>[] listArr = new ArrayList[nodes+1];
+        boolean[] visit = new boolean[nodes+1];
+        
+        for (int i = 1; i <= nodes; i++) {
+            listArr[i] = new ArrayList<Integer>();
         }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            list[start].add(end);
-            list[end].add(start);
+        
+        for (int i = 1; i <= edges; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int now = Integer.parseInt(st.nextToken());
+            int next = Integer.parseInt(st.nextToken());
+            listArr[now].add(next);
+            listArr[next].add(now);
         }
-        int i = 1;
-        Queue<Integer> que = new LinkedList<>();
-        boolean[] visit = new boolean[n+1];
-        visit[i] = true;
-        while(true) {
-            int count = list[i].size();
-            for (int j = 0; j < count; j++) {
-                int temp = list[i].get(j);
-                if(visit[temp] == true) continue;
-                else {
-                    visit[temp] = true;
-                    que.add(temp);
-                }
-            }
-            if (que.size() == 0) break;
-            i = que.poll();
-        }
-        int result = 0;
-        for (int j = 2; j <= n; j++) {
-            if (visit[j] == true) result++;
-        }
-        System.out.println(result);
+        
+        recursive(listArr, visit, 1);
+        System.out.println(count-1);
+       
     }
+    
+    public static void recursive(ArrayList<Integer>[] listArr, boolean[] visit, int now) {
+        
+        if (visit[now]) {
+            return;
+        }
+        
+        visit[now] = true;
+        count++;
+        ArrayList<Integer> list = listArr[now];
+        for (Integer next : list) {
+            recursive(listArr, visit, next);
+        }
+    }
+
+
 }
