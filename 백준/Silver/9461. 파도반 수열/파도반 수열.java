3b2a5f37b1512ce484467de4e1da066e02679131
이주영 <141599648+ljy1348@users.noreply.github.com>
2025-07-17 18:55:40 +0900

    [Silver III] Title: 파도반 수열, Time: 68 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,26 +1,36 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        String answer = "";
-        long[] arr = new long[101];
-        arr[1] = 1;
-        arr[2] = 1;
-        arr[3] = 1;
-        arr[4] = 2;
-        arr[5] = 2;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        long[] dp = new long[101];
+        
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        
         for (int i = 6; i <= 100; i++) {
-            arr[i] = arr[i-1]+arr[i-5];
+            dp[i] = dp[i-1] + dp[i-5];
         }
-        int n = 0;
-        for (int i = 0; i < t; i++) {
-            n = sc.nextInt();
-            if (i == t-1) answer += arr[n];
-            else answer += arr[n]+"\n";
+        
+
+        int numTest = Integer.parseInt(br.readLine());
+
+        while (numTest-- > 0) {
+            int input = Integer.parseInt(br.readLine());
+            sb.append(dp[input]).append("\n");
         }
-        System.out.println(answer);
+
+        System.out.println(sb);
+
+
     }
+
+
 }
