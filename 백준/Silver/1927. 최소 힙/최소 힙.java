e8349701717665c9349126d5e921f087d739db6d
이주영 <141599648+ljy1348@users.noreply.github.com>
2025-07-20 13:54:42 +0900

    [Silver II] Title: 최소 힙, Time: 372 ms, Memory: 46572 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,40 +1,96 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
 
-/**
- * packageName : backjoon
- * fileName : P1927
- * author : GGG
- * date : 2023-10-02
- * description :
- * 요약 :
- * <p>
- * ===========================================================
- * DATE            AUTHOR             NOTE
- * —————————————————————————————
- * 2023-10-02         GGG          최초 생성
- */
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        List<Integer> list = new ArrayList<>();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String result = "";
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (num == 0) {
-                if (queue.isEmpty()) {
-                    System.out.println(0);
+        StringBuilder sb = new StringBuilder();
+
+        int testcase = Integer.parseInt(br.readLine());
+
+        while (testcase-- > 0) {
+            int input = Integer.parseInt(br.readLine());
+
+            if (input == 0) {
+                if (list.size() > 0) {
+
+                    sb.append(list.get(0)).append("\n");
+                    list.set(0, list.get(list.size()-1));
+                    list.remove(list.size()-1);
+
+                    int index = 0;
+
+                    while (list.size() > 1) {
+
+                        int now = list.get(index);
+
+                        int left = Integer.MAX_VALUE;
+                        int right = Integer.MAX_VALUE;
+
+                        if (index*2+1 < list.size()) {
+                            left =  list.get(index*2 + 1);
+                        }
+
+                        if (index*2+2 < list.size()) {
+                            right =  list.get(index*2 + 2);
+                        }
+
+
+
+                        if (left > right && now > right ) {
+                            list.set(index, right);
+                            list.set(index*2 + 2, now);
+                            index = index*2 + 2;
+                        } else if (left <= right && now > left) {
+                            list.set(index, left);
+                            list.set(index*2 + 1, now);
+                            index = index*2 + 1;
+                        } else {
+                            break;
+                        }
+
+                    }
+
+
                 } else {
-                    System.out.println(queue.poll());
+                    sb.append(0).append("\n");
                 }
-            } else {
-                queue.add(num);
             }
+            else {
+
+                if (list.size() == 0) {
+                    list.add(input);
+                } else {
+                    int index = list.size();
+                    list.add(input);
+                    while(true) {
+                        int pIndex = (index-1)/2;
+                        int parent = list.get(pIndex);
+                        if (input < parent) {
+                            list.set(pIndex, input);
+                            list.set(index, parent);
+                        } else {
+                            break;
+                        }
+
+                        index = pIndex;
+                    }
+
+
+                }
+
+            }
+
+
         }
-        System.out.println(result);
+
+        System.out.println(sb.toString());
         br.close();
     }
 }
