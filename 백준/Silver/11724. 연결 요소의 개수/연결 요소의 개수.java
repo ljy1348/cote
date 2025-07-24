46a91dbf046e1223a59f73ba3c35b7e05620ee87
이주영 <141599648+ljy1348@users.noreply.github.com>
2025-07-24 13:46:54 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 564 ms, Memory: 145532 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,61 +1,59 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-/**
- * packageName : backjoon
- * fileName : P11724
- * author : GGG
- * date : 2023-10-02
- * description :
- * 요약 :
- * <p>
- * ===========================================================
- * DATE            AUTHOR             NOTE
- * —————————————————————————————
- * 2023-10-02         GGG          최초 생성
- */
 public class Main {
+
+    static List<List<Integer>> list;
+    static boolean[] visit;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        boolean[] visit = new boolean[n+1];
-        ArrayList<Integer>[] list = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) {
-            list[i] = new ArrayList<>();
+        int nodeNum = Integer.parseInt(st.nextToken());
+        int edgeNum = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[nodeNum+1];
+
+        list = new ArrayList<>();
+
+        while (nodeNum-- >= 0) {
+            list.add(new ArrayList<>());
         }
-        for (int i = 0; i < m; i++) {
+
+        while (edgeNum-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int idx = Integer.parseInt(st.nextToken());
-            int val = Integer.parseInt(st.nextToken());
-            list[idx].add(val);
-            list[val].add(idx);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list.get(u).add(v);
+            list.get(v).add(u);
         }
-        for (int i = 1; i <= n; i++) {
-        if (visit[i]) continue;
-        count++;
-        func(visit,list,i);
-        }
-        System.out.println(count);
-    }
 
+        int result = 0;
 
-    static int count = 0;
-    static void func(boolean[] visit, ArrayList<Integer>[] list, int num) {
-        visit[num] = true;
-        if (list[num].isEmpty()) {return;}
-        while(list[num].size() > 0) {
-            int temp = list[num].get(0);
-            list[num].remove(0);
-            if (visit[temp]) continue;
-            else {
-                func(visit,list,temp);
+        for (int i = 1; i < visit.length; i++) {
+            if (dfs(i)) {
+                result++;
             }
         }
+
+        System.out.println(result);
+
     }
+
+    static boolean dfs(int node) {
+        if (visit[node]) {
+            return false;
+        }
+
+        visit[node] = true;
+
+        for (int next : list.get(node)) {
+            dfs(next);
+        }
+        return true;
+    }
+    
 }
